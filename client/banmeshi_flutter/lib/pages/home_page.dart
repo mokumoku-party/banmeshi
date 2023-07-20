8f9dc758c93a1b5e131ae2c0f00b5938777b0d0e
dicenull <dicenull.dev@gmail.com>
2023-07-20 21:29:13 +0900

    ✨ ホームページを追加

---
+++ client/banmeshi_flutter/lib/pages/home_page.dart
@@ -0,0 +1,16 @@
+import 'package:banmeshi_flutter/model/user_controller.dart';
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class HomePage extends HookConsumerWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final user = ref.watch(userProvider);
+
+    return Scaffold(
+      body: Text('ようこそ ${user?.name}'),
+    );
+  }
+}
--- client/banmeshi_flutter/lib/routes/app_router.dart
+++ client/banmeshi_flutter/lib/routes/app_router.dart
@@ -1,3 +1,5 @@
+import 'package:banmeshi_flutter/model/user_controller.dart';
+import 'package:banmeshi_flutter/pages/home_page.dart';
 import 'package:banmeshi_flutter/pages/login_page.dart';
 import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -7,8 +9,20 @@ final appRouterProvider = Provider(
     routes: [
       GoRoute(
         path: '/',
-        builder: (context, state) => const LoginPage(),
+        builder: (context, state) => const HomePage(),
+        redirect: (context, state) {
+          final user = ref.read(userProvider);
+          if (user == null) {
+            return '/login';
+          }
+
+          return null;
+        },
       ),
+      GoRoute(
+        path: '/login',
+        builder: (context, state) => const LoginPage(),
+      )
     ],
   ),
 );
