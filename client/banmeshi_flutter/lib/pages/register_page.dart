1b6f910565b1161db2ef769deab6fa2b4c90aa29
dicenull <dicenull.dev@gmail.com>
2023-08-31 22:03:39 +0900

    ✨ 食材登録画面のUI更新

--- client/banmeshi_flutter/lib/pages/register_page.dart
+++ client/banmeshi_flutter/lib/pages/register_page.dart
@@ -1,3 +1,5 @@
+import 'package:banmeshi_flutter/pages/home_page.dart';
+import 'package:banmeshi_flutter/routes/app_router.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
@@ -6,8 +8,45 @@ class RegisterPage extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return const Scaffold(
-      body: Text('とうろく'),
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(32),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Padding(
+                padding: EdgeInsets.all(16),
+                child: Text('ざいこ'),
+              ),
+              Flexible(
+                child: ListView(
+                  children: rowList
+                      .map(
+                        (row) => Card(
+                          child: Padding(
+                            padding: const EdgeInsets.all(16),
+                            child: Text('${row.name}: ${row.amount}'),
+                          ),
+                        ),
+                      )
+                      .toList(),
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  ref.read(appRouterProvider).go('/');
+                },
+                child: const Padding(
+                  padding: EdgeInsets.all(16),
+                  child: Text('完成'),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
