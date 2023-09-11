ba703625e50226490bcf42fc0dad905e33c42b26
Ryo Hodota <45889934+Ryo-7@users.noreply.github.com>
2023-09-11 12:32:59 +0900

    サービスをサーバに登録 (#8)
    * 一旦全部のサービスを登録（返す値は適当）
    
    * 生成したコードのパスがおかしいので修正
    
    * 消えたプロトから生成されたコードが残っていたので、削除

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -17,6 +17,9 @@ import (
 
 type InventoryServer struct {}
 
+type RecipeServer struct {}
+
+
 func (s *InventoryServer) FetchInventory (ctx context.Context, req *connect.Request[grpc.User]) (*connect.Response[service.Inventory], error) {
 	resArray := []*grpc.Ingredient{}
 	resStruct := &grpc.Ingredient{
@@ -36,10 +39,62 @@ func (s *InventoryServer) AddInventory (ctx context.Context, req *connect.Reques
 	return res,nil
 }
 
+func (s *RecipeServer) FetchRecipe (ctx context.Context, req *connect.Request[grpc.User]) (*connect.Response[grpc.Food],error){
+	resArray := []*grpc.Ingredient{}
+	resStruct := &grpc.Ingredient{
+		Name:fmt.Sprintf("hoge"),
+		Amount:1,
+		Unit:0,
+		RegisterDate:0,
+	}
+	resArray = append(resArray,resStruct)
+	res := connect.NewResponse(&grpc.Food{
+		Name: fmt.Sprintf("sample_recipe_name"),
+		Serving: 1,
+		Ingredient: resArray,
+		ReferenceUrl: "hogehoge.com",
+		})
+	return res,nil
+}
+
+func (s *RecipeServer) SelectRecipe (ctx context.Context, req *connect.Request[service.Recipe]) (*connect.Response[grpc.Void],error){
+	res := connect.NewResponse(&grpc.Void{})
+	return res, nil
+}
+
+func (s *RecipeServer) RegisterFoodAsRecipe (ctx context.Context, req *connect.Request[service.Recipe]) (*connect.Response[grpc.Void], error){
+	res := connect.NewResponse(&grpc.Void{})
+	return res, nil
+}
+
+func (s *RecipeServer) FetchRecommendRecipe (ctx context.Context, req *connect.Request[grpc.User]) (*connect.Response[service.RecommendFood], error){
+	resArray := []*grpc.Ingredient{}
+	resStruct := &grpc.Ingredient{
+		Name:fmt.Sprintf("hoge"),
+		Amount:1,
+		Unit:0,
+		RegisterDate:0,
+	}
+	resArray = append(resArray,resStruct)
+	foodArray := []*grpc.Food{}
+	food := &grpc.Food{
+		Name: fmt.Sprintf("sample_recipe_name"),
+		Serving: 1,
+		Ingredient: resArray,
+		ReferenceUrl: "hogehoge.com",
+	}
+	foodArray = append(foodArray,food)
+	res := connect.NewResponse(&service.RecommendFood{ RecommendFoods: foodArray})
+	return res, nil
+}
+
 func main() {
 	inventoryServer := &InventoryServer{}
+	recipeServer := &RecipeServer{}
 	mux := http.NewServeMux()
 	path, handler := serviceconnect.NewInventoryServiceHandler(inventoryServer)
+	mux.Handle(path, handler)
+	path, handler = serviceconnect.NewRecipeServiceHandler(recipeServer)
 	mux.Handle(path, handler)
 	http.ListenAndServe(
 		"localhost:8080",
