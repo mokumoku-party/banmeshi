e13f57c9b4c75b44066fbb1c904b0127f37d2789
Ryo-7 <ryo07011999@yahoo.co.jp>
2023-09-12 21:10:17 +0900

    フォーマットがかかった

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -3,10 +3,11 @@ package main
 import (
 	"net/http"
 
+	"context"
+	"fmt"
+
 	"golang.org/x/net/http2"
 	"golang.org/x/net/http2/h2c"
-	"fmt"
-	"context"
 
 	"github.com/bufbuild/connect-go"
 
@@ -15,76 +16,74 @@ import (
 	"github.com/mokumoku-party/banmeshi/server/pkg/grpc/service/serviceconnect"
 )
 
-type InventoryServer struct {}
+type InventoryServer struct{}
 
-type RecipeServer struct {}
+type RecipeServer struct{}
 
-
-func (s *InventoryServer) FetchInventory (ctx context.Context, req *connect.Request[grpc.User]) (*connect.Response[service.Inventory], error) {
+func (s *InventoryServer) FetchInventory(ctx context.Context, req *connect.Request[grpc.User]) (*connect.Response[service.Inventory], error) {
 	resArray := []*grpc.Ingredient{}
 	resStruct := &grpc.Ingredient{
-		Name:fmt.Sprintf("hoge"),
-		Amount:1,
-		Unit:0,
-		RegisterDate:0,
+		Name:         fmt.Sprintf("hoge"),
+		Amount:       1,
+		Unit:         0,
+		RegisterDate: 0,
 	}
-	resArray = append(resArray,resStruct)
-	res := connect.NewResponse(&service.Inventory{Ingredients: resArray,
-    })
-	return res,nil
+	resArray = append(resArray, resStruct)
+	res := connect.NewResponse(&service.Inventory{Ingredients: resArray})
+	return res, nil
 }
 
-func (s *InventoryServer) AddInventory (ctx context.Context, req *connect.Request[service.Item]) (*connect.Response[grpc.Void], error){
+func (s *InventoryServer) AddInventory(ctx context.Context, req *connect.Request[service.Item]) (*connect.Response[grpc.Void], error) {
 	res := connect.NewResponse(&grpc.Void{})
-	return res,nil
+	return res, nil
 }
 
-func (s *RecipeServer) FetchRecipe (ctx context.Context, req *connect.Request[grpc.User]) (*connect.Response[grpc.Food],error){
+func (s *RecipeServer) FetchRecipe(ctx context.Context, req *connect.Request[grpc.User]) (*connect.Response[grpc.Food], error) {
 	resArray := []*grpc.Ingredient{}
 	resStruct := &grpc.Ingredient{
-		Name:fmt.Sprintf("hoge"),
-		Amount:1,
-		Unit:0,
-		RegisterDate:0,
+		Name:         fmt.Sprintf("hoge"),
+		Amount:       1,
+		Unit:         0,
+		RegisterDate: 0,
 	}
-	resArray = append(resArray,resStruct)
+	resArray = append(resArray, resStruct)
 	res := connect.NewResponse(&grpc.Food{
-		Name: fmt.Sprintf("sample_recipe_name"),
-		Serving: 1,
-		Ingredient: resArray,
+		Name:         fmt.Sprintf("sample_recipe_name"),
+		Serving:      1,
+		Ingredient:   resArray,
 		ReferenceUrl: "hogehoge.com",
-		})
-	return res,nil
+	})
+	return res, nil
 }
 
-func (s *RecipeServer) SelectRecipe (ctx context.Context, req *connect.Request[service.Recipe]) (*connect.Response[grpc.Void],error){
+func (s *RecipeServer) SelectRecipe(ctx context.Context, req *connect.Request[service.Recipe]) (*connect.Response[grpc.Void], error) {
 	res := connect.NewResponse(&grpc.Void{})
 	return res, nil
 }
 
-func (s *RecipeServer) RegisterFoodAsRecipe (ctx context.Context, req *connect.Request[service.Recipe]) (*connect.Response[grpc.Void], error){
+func (s *RecipeServer) RegisterFoodAsRecipe(ctx context.Context, req *connect.Request[service.Recipe]) (*connect.Response[grpc.Void], error) {
 	res := connect.NewResponse(&grpc.Void{})
 	return res, nil
 }
 
-func (s *RecipeServer) FetchRecommendRecipe (ctx context.Context, req *connect.Request[grpc.User]) (*connect.Response[service.RecommendFood], error){
+func (s *RecipeServer) FetchRecommendRecipe(ctx context.Context, req *connect.Request[grpc.User]) (*connect.Response[service.RecommendFood], error) {
 	resArray := []*grpc.Ingredient{}
 	resStruct := &grpc.Ingredient{
-		Name:fmt.Sprintf("hoge"),
-		Amount:1,
-		Unit:0,
-		RegisterDate:0,
+		Name:         fmt.Sprintf("hoge"),
+		Amount:       1,
+		Unit:         0,
+		RegisterDate: 0,
 	}
-	resArray = append(resArray,resStruct)
+	resArray = append(resArray, resStruct)
 	foodArray := []*grpc.Food{}
 	food := &grpc.Food{
-		Name: fmt.Sprintf("sample_recipe_name"),
-		Serving: 1,
-		Ingredient: resArray,
+		Name:         fmt.Sprintf("sample_recipe_name"),
+		Serving:      1,
+		Ingredient:   resArray,
 		ReferenceUrl: "hogehoge.com",
 	}
-	foodArray = append(foodArray,food)
-	res := connect.NewResponse(&service.RecommendFood{ RecommendFoods: foodArray})
+	foodArray = append(foodArray, food)
+	res := connect.NewResponse(&service.RecommendFood{RecommendFoods: foodArray})
 	return res, nil
 }
 
