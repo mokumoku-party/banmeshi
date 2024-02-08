32342d9a0778c64600d1b045727332925b3a5624
dicenull <dicenull.dev@gmail.com>
2024-02-08 21:41:20 +0900

    ✨ リコメンドのProviderと仮画面を追加

--- client/banmeshi_flutter/lib/pages/recommend_page.dart
+++ client/banmeshi_flutter/lib/pages/recommend_page.dart
@@ -1,3 +1,4 @@
+import 'package:banmeshi_flutter/model/recommend_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
@@ -6,6 +7,33 @@ class RecommendPage extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return const Scaffold(body: Text('りこめんど'));
+    return const Scaffold(
+      body: _Body(),
+    );
+  }
+}
+
+class _Body extends HookConsumerWidget {
+  const _Body();
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final result = ref.watch(recommendProvider).asData?.value;
+
+    if (result == null) return const CircularProgressIndicator();
+
+    final foods = result.recommendFoods;
+
+    return ListView.builder(
+      itemCount: foods.length,
+      itemBuilder: (BuildContext context, int index) {
+        final food = foods[index];
+
+        return Container(
+          padding: const EdgeInsets.all(32),
+          child: Text(food.ingredient.join(",") + food.name),
+        );
+      },
+    );
   }
 }
